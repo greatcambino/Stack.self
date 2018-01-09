6f5ecb4985fce9c9d655231699f1a2c14e7cf213
TERRENCE CAMPBELL <TLCAMPBELL89@GMAIL.COM>
2018-01-09 14:23:42 -0500

    heroku with node almost on deck

--- db/connection.js
+++ db/connection.js
@@ -4,4 +4,10 @@ mongoose.Promise = Promise
 
 mongoose.connect('mongodb://localhost/stack-me', {useMongoClient: true})
 
+if (process.env.NODE_ENV == "production") {
+    mongoose.connect(process.env.MLAB_URL)
+  } else {
+    mongoose.connect("mongodb://localhost/whenpresident");
+  }
+
 module.exports = mongoose
